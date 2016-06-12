12162811f21947d72451e5d3b99548bcfb8b892e
Paolo P <paolo.pareti@gmail.com>
2016-06-12 18:09:37 +0100

    update

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ printlog:true,
 timeout:10000,
 proxy:"http://paolopareti.uk/proxy/get.php?url=",
 url:"http://paolopareti.uk/prohow/prohow.ttl",
-seed_urls:["http://paolopareti.uk/prohow/prohow.ttl", "http://paolopareti.uk/prohow/prohossssssw.ttl"]
+seed_urls:["http://paolopareti.uk/prohow/prohow.ttl", "http://paolopareti.uk/prohow/prohow.rdf", "http://paolopareti.uk/prohow/prohossssssw.ttl"]
 };
 
 //var initp = {
--- khjsclient.js
+++ khjsclient.js
@@ -1,5 +1,3 @@
-$(function(){ 
-// start on dom ready
 var FOAF = $rdf.Namespace("http://xmlns.com/foaf/0.1/")
 var RDF = $rdf.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 var RDFS = $rdf.Namespace("http://www.w3.org/2000/01/rdf-schema#")
@@ -14,18 +12,94 @@ var log = function(text){
 	logc(text,"log_default");
 }
 var logc = function(text,type){
-	$("#debuglog").append("<tr class=\"logtable\"><td class=\"logtable\">"+(new Date().toISOString())+"</td><td class=\"logtable\, "+type+"\">"+text+"</td></tr>");
+	$("#debuglog").prepend("<tr class=\"logtable\"><td class=\"logtable\">"+(new Date().toISOString())+"</td><td class=\"logtable\, "+type+"\">"+text+"</td></tr>");
 }
+var print_dataset = function(){
+	$("#rdf_dump").html();
+	$rdf.fetcher = null;
+	kb.fetcher = null;
+	var q = $rdf.SPARQLToQuery("SELECT ?a ?b ?c WHERE { ?a ?b ?c . } ", false, kb);
+	var result = kb.query(q, function(result,results) {
+		var a = printResult(result['?a']);//.uri ? result['?a'].uri : result['?a'].toString();
+		var b = printResult(result['?b']);
+		var c = printResult(result['?c']);
+        //results += " -->"+(result['?c']) ? result['?c'].value : '?';
+		if(!((isOther(result['?a'])) || (isOther(result['?b'])) || (isOther(result['?c']))))
+			print_rdf(a+" "+b+" "+c+" . ");
+		else print_rdf("# Non standard triple: "+a+" "+b+" "+c+" . ");
+    });
+}
+var print_rdf = function(text){
+	$("#rdf_dump").append(text+"<br>");
+}
+
+var printResult = function(result){
+	if(isBlank(result)) return escapeHtml(" _:"+result.id);
+	if(isLiteral(result)) return escapeHtml("\"\"\""+result.value+"\"\"\"");
+	if(isURI(result)) return escapeHtml("<"+result.uri+">");
+	return "ERROR";
+}
+
+var isBlank = function(object){
+	if((typeof object.id !== 'undefined') && (typeof object.value !== 'undefined')) return true;
+	return false;
+}
+var isLiteral = function(object){
+	if((typeof object.id == 'undefined') && (typeof object.value !== 'undefined')) return true;
+	return false;
+}
+var isURI = function(object){
+	if((typeof object.uri !== 'undefined')) return true;
+	return false;
+}
+var isOther = function(object){
+	if(isBlank(object) || isLiteral(object) || isURI(object)) return false;
+	return true;
+}
+var escapeHtml =  function(unsafe) {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ }
+var open_log = function(){
+	$("#open_log_button").hide();
+	$("#close_log_button").show();
+	$("#log_div").show();
+}
+var open_rdf_dump = function(){
+	$("#open_rdf_dump_button").hide();
+	$("#close_rdf_dump_button").show();
+	print_dataset();
+	$("#rdf_dump_wrapper").show();
+}
+var close_log = function(){
+	$("#open_log_button").show();
+	$("#close_log_button").hide();
+	$("#log_div").hide();
+}
+var close_rdf_dump = function(){
+	$("#open_rdf_dump_button").show();
+	$("#close_rdf_dump_button").hide();
+	$("#rdf_dump").html();
+	$("#rdf_dump_wrapper").hide();
+}
+//////
+
+
+$(function(){ 
+// start on dom ready
 if(typeof initp.printlog !== 'undefined' && initp.printlog){
-	var pre="INITIAL PARAM ";
-	var post=": ";
+	var pre="Initialize parameter \"";
+	var post="\": ";
 	for (var property in initp) {
 		if (initp.hasOwnProperty(property) && typeof initp[property] !== 'undefined') {
 			logc(pre+property+post+initp[property],"log_misc");
 		}
 	}
 }
-//////
 
 // DATA FETCHING
 
@@ -40,37 +114,13 @@ var fetch_data = function(url, fetcher,callback){
 				logc("Fetching data from "+url+" FAILED ("+xhr.status+").","log_error");
 			} else {
 				logc("Fetching data from "+url+" SUCCEEDED ("+xhr.status+").","log_success");
-				callback();
+				if(typeof callback !== 'undefined') callback();
 			}
 		});
 	}
 	catch(err){
 		logc("Fetching data from "+url+" FAILED.","log_error");
 	}
-
-	
-	
-	//if(typeof initp.proxy !== 'undefined'){
-	//	log("Fetching data from "+initp.proxy+url+".");
-		//fetcher.nowOrWhenFetched(initp.proxy+url, undefined, function(ok, body, xhr) {
-		//	logc("ok","log_success");
-	//		logc("body","log_success");
-	//		logc("xhr","log_success");
-	//		try {
-	//				if (!ok) {
-		//				logc("Fetching data from "+url+" FAILED.","log_error");
-		//				return false;
-		//			} else {
-		//				logc("Fetching data from "+url+" SUCCEEDED through proxy "+initp.proxy+".","log_success");
-		//				callback();
-		//				return true;
-		//			}
-		//	}
-		//	catch(err){
-		//	logc("Fetching data from "+url+" FAILED.","log_error");
-	//		}
-	//	});
-//} 
 };
 //////
 
@@ -97,7 +147,7 @@ var load_seed_uris = function(){
 	if(typeof initp.seed_urls !== 'undefined'){
 		logc("Loading seed data","log_misc");
 		for (var i = 0; i < initp.seed_urls.length; i++) {
-			fetch_data(initp.seed_urls[i],fetch,test);
+			fetch_data(initp.seed_urls[i],fetch);
 		}
 	}
 }
@@ -106,7 +156,7 @@ var load_seed_uris = function(){
 
 // MAIN CODE
 
-var kb = $rdf.graph();
+kb = $rdf.graph();
 if(typeof initp.proxy !== 'undefined') {
 	if(initp.proxy.indexOf("{uri}") > 0) {
 		$rdf.Fetcher.crossSiteProxyTemplate = initp.proxy;
@@ -117,7 +167,7 @@ if(typeof initp.proxy !== 'undefined') {
 		logc("Proxy "+initp.proxy+" does not have the required URI placeholder \"{uri}\", please add it in the config/config.js file. It has now been interpreted as "+initp.proxy+"{uri}" ,"log_warning");
 	}
 }
-var fetch = $rdf.fetcher(kb,initp.timeout);
+fetch = $rdf.fetcher(kb,initp.timeout);
 //$rdf.Fetcher.crossSiteProxy(initp.proxy);
 load_seed_uris();
 
